9940c2fe1316c391bc3e91224d7c14c9f000ea9a
DUPRE Théo <theo.thws.06@gmail.com>
2025-01-22 15:19:22 +0100

    Affichage conditionnel avec switch

--- exercices.js
+++ exercices.js
@@ -1,3 +1,6 @@
+const nom_sorcier = "Esiah";
+console.log("Le sorcier se nomme :" + nom_sorcier + " ! 🎉");
+
 const nom_boutique = "YoupiShop";
 let potion_de_soin = 3;
 let prix_potion_de_soin = 5;
@@ -11,3 +14,27 @@ else
     nom_boutique,
     "est fermée, revenez plus tard Aventurier ! `😴`"
   );
+
+console.log(
+  "Bienvenue dans mon humble boutique Aventurier. Que veux-tu savoir? `🤔`\n1. Le nom de la boutique\n2. Le nom du sorcier\n3. Le prix d'une potion de soin\n4. La quantité d'une potion de soin"
+);
+let choix = parseInt(prompt("Choix (1,2,3,4): "));
+switch (choix) {
+  case 1:
+    console.log("Nom de la boutique: ", nom_boutique);
+    break;
+  case 2:
+    console.log("Nom du sorcier: ", nom_sorcier);
+    break;
+  case 3:
+    console.log("Prix de la potion de soin: ", prix_potion_de_soin);
+    break;
+  case 4:
+    console.log("Quantité de potion de soin: ", potion_de_soin);
+    break;
+  default:
+    console.log(
+      `Mh... Désolé aventurier, je ne comprends pas ce que tu souhaites. Refais ton choix ! 😕`
+    );
+    break;
+}
