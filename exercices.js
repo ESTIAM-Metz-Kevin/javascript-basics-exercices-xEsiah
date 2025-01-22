13024f9c7409aaa7364675cf13673fce9ca19beb
DUPRE Théo <theo.thws.06@gmail.com>
2025-01-22 14:53:39 +0100

    Affichage conditionnel

--- exercices.js
+++ exercices.js
@@ -2,3 +2,12 @@ const nom_boutique = "YoupiShop";
 let potion_de_soin = 3;
 let prix_potion_de_soin = 5;
 let statut_boutique_ouverte = true;
+
+if (statut_boutique_ouverte === true)
+  console.log("Bienvenue dans la boutique", nom_boutique, "Aventurier!`🎉`");
+else
+  console.log(
+    "La boutique",
+    nom_boutique,
+    "est fermée, revenez plus tard Aventurier ! `😴`"
+  );
